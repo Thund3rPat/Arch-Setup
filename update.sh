7e8b4de9dff9399a14979a91d60c2704988c73d4
Thund3rPat <patricksimuttis@gmail.com>
2020-05-07 17:10:02 +0200

    add neofetch

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ cp -r ~/.config/kitty/ config/
 cp -r ~/.config/picom/ config/
 cp -r ~/.config/polybar/ config/
 cp -r ~/.config/sxhkd/ config/
+cp -r ~/.config/neofetch/ config/
 
 cp ~/.zshrc config/
 
